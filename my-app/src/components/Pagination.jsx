eff2e8bb3427d2add16de46d189f7b75b9edd828
hafizhbimo <hfzhbimo@gmail.com>
2024-07-23 14:07:16 +0700

    add pagination component

---
+++ my-app/src/components/Pagination.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function Pagination ({count, next, previous, setOffset, offset, limit}){
+    const [currentPage, setCurrentPage]= useState(1)
+    const maxPage = Math.ceil(count / 20);
+    const onClick = (type)=>{
+        switch(type){
+            case 'next':return setOffset(offset+20)
+            case 'previous':return  setOffset(offset-20) 
+            case `${type}`: return setOffset((type-1)*limit)
+            default: return
+        }
+        
+    }
+    return(
+<div class="flex">
+  <button disabled={offset==0} onClick={()=>onClick('previous')} className="flex items-center justify-center px-4 h-10 me-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+    <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+    </svg>
+    Previous
+  </button>
+  <button disabled={offset==(maxPage*20)} onClick={()=>onClick('next')} className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" >
+    Next
+    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg>
+  </button>
+</div>
+    )
+}
