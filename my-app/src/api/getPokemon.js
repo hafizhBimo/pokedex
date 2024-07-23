af1fd7a5985ac3bff9c9e6ac2db3c53ae12abf93
hafizhbimo <hfzhbimo@gmail.com>
2024-07-23 14:06:57 +0700

    add getPokemon api

--- my-app/src/api/getPokemon.js
+++ my-app/src/api/getPokemon.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default function getPokemon() {
-  return axios.get(`https://pokeapi.co/api/v2/pokemon`);
+export default function getPokemon(limit,offset) {
+  return axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
 }
+
+
